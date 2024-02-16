e2287e969bc2bf2e3ae38003ca657659842e7403
DKHOA111 <dkhoa0120@gmail.com>
2024-02-16 17:44:49 +0700

    change font

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,11 @@ const Footer = () => {
   return (
     <section
       id="footer"
-      className="max-container gap-32 justify-between flex max-sm:flex-col"
+      className="max-container gap-20 justify-between flex max-sm:flex-col"
     >
       <div className="flex flex-col gap-3 max-w-sm">
         <img src="./img/footer.PNG" alt="footer" className="w-[200px]" />
-        <p className="info-text">
+        <p className="text-footer">
           Established in 1995, our passion and mission for footwear transcends
           mere functionality. It is about making a statement with every step you
           take.
@@ -25,29 +25,29 @@ const Footer = () => {
         </div>
       </div>
       <div className="flex flex-col gap-4 mt-4">
-        <h2>Brands</h2>
+        <h3>Brands</h3>
         <div>
-          <p className="info-text">Nike</p>
-          <p className="info-text">Adidas</p>
-          <p className="info-text">Puma</p>
-          <p className="info-text">SKC</p>
+          <p className="text-footer">Nike</p>
+          <p className="text-footer">Adidas</p>
+          <p className="text-footer">Puma</p>
+          <p className="text-footer">SKC</p>
         </div>
       </div>
       <div className="flex flex-col gap-4 mt-4">
-        <h2>Help</h2>
+        <h3>Help</h3>
         <div>
-          <p className="info-text">About Us</p>
-          <p className="info-text">FAQs</p>
-          <p className="info-text">How it works</p>
-          <p className="info-text">Privacy Policy</p>
-          <p className="info-text">Payment Policy</p>
+          <p className="text-footer">About Us</p>
+          <p className="text-footer">FAQs</p>
+          <p className="text-footer">How it works</p>
+          <p className="text-footer">Privacy Policy</p>
+          <p className="text-footer">Payment Policy</p>
         </div>
       </div>
       <div className="flex flex-col gap-4 mt-4">
-        <h2>Get in touch</h2>
+        <h3>Get in touch</h3>
         <div>
-          <p className="info-text">admin3k@gmail.com</p>
-          <p className="info-text">+84 81239771</p>
+          <p className="text-footer">admin3k@gmail.com</p>
+          <p className="text-footer">+84 81239771</p>
         </div>
       </div>
     </section>
